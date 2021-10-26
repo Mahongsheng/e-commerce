a198231d876b5ae74d353eae75c38b8846e48540
mahongsheng <mahongsheng_cs@163.com>
2021-10-26 22:07:18 +0800

    将注册中心接入Eureka

--- e-commerce-admin/src/main/java/com/ecommerce/controller/GitController.java
+++ e-commerce-admin/src/main/java/com/ecommerce/controller/GitController.java
@@ -2,10 +2,12 @@ package com.ecommerce.controller;
 
 import com.ecommerce.config.GitAutoRefreshConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GitController {
 
     @Autowired
